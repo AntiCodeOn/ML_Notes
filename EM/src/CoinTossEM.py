ceee30073a94a5f0532b50fc043d9be0554d9dc9
anticodeon <anticodeon@gmail.com>
2017-10-18 23:40:04 +0200

    Working implementation of EM algorithm, needs refactoring

--- EM/src/CoinTossEM.py
+++ EM/src/CoinTossEM.py
@@ -1,36 +1,66 @@
-#[[int(letter=='H') for letter in word] for word in words]
 
 import numpy as np
 import operator
 from scipy import stats
-import sympy as sp
+#import sympy as sp
 
 #true (unknown theta values)
 Theta = [0.8, 0.45]
 #true (unknown coin selections)
 coins  = ['B', 'A', 'A', 'B', 'A']
 
+def bernoulli(theta, experiment):
+	return (theta**experiment[0])*(1-theta)**experiment[1]
+
+def prob_x_theta(theta, experiment):
+	return [bernoulli(theta[0], experiment), bernoulli(theta[1], experiment)]
+
+def expect_zk(theta, experiment):
+	ex = prob_x_theta(theta, experiment)
+	ex_den = (ex[0]+ex[1])
+	return [ex[0]/ex_den, ex[1]/ex_den]
+
+def max_likelihood(E, bin_events):
+	numTheta0=bin_events*E[:,0:1]
+	numTheta1=bin_events*E[:,1:]
+	den=10*np.sum(E, axis=0)
+	nomTheta0= np.sum(numTheta0)
+	nomTheta1= np.sum(numTheta1)
+	return [nomTheta0/den[0], nomTheta1/den[1]]
+	
+	
+
 events = (['HTTTHHTHTH',
           'HHHHTHHHHH',
           'HTHHHHHTHH',
           'HTHTTTHHTT',
           'THHHTHHHTH'])
-bin_events = [[ for event in events]
+bin_events = np.array([[int(letter=='H') for letter in word] for word in events])
 Total = len(events[0])
 
-heads = [event.count('H') for event in events]
+heads = np.array([event.count('H') for event in events])
+tails = Total - heads
+experiments = list(zip(heads, tails))
 print(coins)
 print(heads)
 
-#random theta guess
-rTheta = [0.6, 0.7]
 
-t, T, s = sp.symbols('theta, T, s')
-likelihood = (t**s)*(1-t)**(T-s)
-_likelihood = sp.lambdify((t,T,s), likelihood, modules='numpy')
-Ez = [0, 0]
-i = 0
-for j in range(events):
-   Ez[i]=_likelihood(rTheta[i], Total, heads[j])
-   Ez[i+1]=_likelihood(rTheta[i+1], Total, heads[j])
+#random theta guess
+rTheta = [0.6, 0.5]
+
+#t, T, s = sp.symbols('theta, T, s')
+#likelihood = (t**s)*(1-t)**(T-s)
+#_likelihood = sp.lambdify((t,T,s), likelihood, modules='numpy')
+
+for i in range(10):
+	E = np.array([expect_zk(rTheta, experiment) for experiment in experiments])
+	#print(' ')
+	#print(E)
+	#print(' ')
+	#print(bin_events)
+
+	
+	rTheta = max_likelihood(E, bin_events)
+	
+print(rTheta)
 
