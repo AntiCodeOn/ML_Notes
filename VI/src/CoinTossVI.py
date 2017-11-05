f583a8c72e173cd6ca796ed08719ad796e5d10d3
anticodeon <anticodeon@gmail.com>
2017-11-05 22:43:22 +0100

    Added ELBO function, there is a bug in this version

--- VI/src/CoinTossVI.py
+++ VI/src/CoinTossVI.py
@@ -25,7 +25,7 @@ dirichlet_param = np.array([[5, 5]])
 r_nk = np.ones((5,2))
 ro_nk = 2*np.ones((5,2))
 beta_k1 = np.array([2, 2])
-beta_k2 = np.array([5, 2])
+beta_k2 = np.array([5, 3])
 beta_param = np.column_stack((np.transpose(beta_k1), np.transpose(beta_k2)))
 
 def calc_ro(outcomes, dirichlet_param, beta_param):
@@ -64,10 +64,28 @@ def update_beta_param2(beta_param, r_nk, outcomes):
 	beta_update = np.stack((a_update, b_update)).reshape(beta_param.shape)
 	return beta_param + beta_update
 
+def exp_p_theta(beta_param):
+	beta_a = beta_param[0,:]
+	beta_b = beta_param[1,:]
+	p_theta_k = special.psi(beta_a) + special.psi(beta_a+beta_b)
+	p_theta = np.sum(p_theta_k)
+	return p_theta
+
+def exp_p_pi(dirichlet_param):
+	alpha_tilde = np.sum(dirichlet_param)
+	exp_pi = np.sum((1 - dirichlet_param)*(special.psi(dirichlet_param) - special.psi(alpha_tilde)))
+	C_alpha =  special.gamma(alpha_tilde)/np.prod(special.gamma(dirichlet_param))
+	return np.log(C_alpha)+exp_pi
+	
+
+def ELBO(dirichlet_old, beta_old, dirichlet_new, beta_new):
+	elbo = exp_p_theta(beta_old) - exp_p_theta(beta_new) + exp_p_pi(dirichlet_old) - exp_p_pi(dirichlet_new)
+	return elbo
+
 #print(dirichlet_param)
 #print(np.shape(dirichlet_param))
 #print(r_nk)
-for i in range(15):
+for i in range(1000):
 	ro_nk = calc_ro(experiments, dirichlet_param, beta_param)
 	print("ro_nk: ", ro_nk)
 	r_nk = calc_r(ro_nk)
@@ -76,5 +94,6 @@ for i in range(15):
 	print("dirichlet_new: ", dirichlet_new)
 	beta_new = update_beta_param2(beta_param, r_nk, experiments)
 	print("beta_new: ", beta_new)
+	print("delta ", i, ": ", ELBO(dirichlet_param, beta_param, dirichlet_new, beta_new))
 	dirichlet_param = dirichlet_new
 	beta_param = beta_new
