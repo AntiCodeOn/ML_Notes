63ffa586c8b13aa3d42a7c0d542005a8fd9090a5
anticodeon <anticodeon@gmail.com>
2017-11-09 23:51:40 +0100

    Add unittest template

---
+++ VI/src/test_CoinTossVI.py
@@ -0,0 +1,17 @@
+import unittest
+import numpy as np
+import scipy.stats
+from scipy import stats, special
+
+
+class Test(unittest.TestCase):
+	def test_1(self):
+		#self.assertEqual(a,b)
+		#self.assertGreaterEqual(a,b)
+		#self.assertAlmostEqual(a,b)
+		pass
+	def test2(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
